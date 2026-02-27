0af762dbe170bf82bb72527a9cef10e284aed63d
atik806 <atikrj8@gmail.com>
2026-02-28 02:07:14 +0600

    v2

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 from flask import Flask, render_template
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 app = Flask(__name__)
+app.wsgi_app = ProxyFix(app.wsgi_app)
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/<path:path>')
+def static_files(path):
+    return app.send_static_file(path)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
