db61679b6a41bae8e4e207253a27f343d13f191a
atik806 <atikrj8@gmail.com>
2026-03-02 01:37:36 +0600

    update last 4

---
+++ api/index.py
@@ -0,0 +1,38 @@
+import os
+from flask import Flask, render_template
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app = Flask(
+    __name__,
+    template_folder=os.path.join(basedir, "..", "templates"),
+    static_folder=os.path.join(basedir, "..", "static")
+)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/cart")
+def cart():
+    return render_template("cart.html")
+
+@app.route("/checkout")
+def checkout():
+    return render_template("checkout.html")
+
+@app.route("/admin")
+def admin():
+    return render_template("admin.html")
+
+@app.route("/product")
+def product():
+    return render_template("product.html")
